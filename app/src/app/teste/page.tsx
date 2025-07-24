51e12282288e983ec8cd3613c962c77fdb912954
albertorcf <albertorcf@gmail.com>
2025-07-24 17:52:08 -0300

    Remove Redux provider and related store configuration from TestePage and delete unused raqbConfig and store files

--- app/src/app/teste/page.tsx
+++ app/src/app/teste/page.tsx
@@ -2,16 +2,13 @@
 'use client';
 
 import { Provider } from "react-redux";
-import { store } from "@/components/QueryBuilder/store";
 import QueryBuilder from "@/components/QueryBuilder/MyQueryBuilder";
 
 export default function TestePage() {
   return (
-    <Provider store={store}>
-      <div style={{ maxWidth: 800, margin: '40px auto', padding: 24 }}>
-        <h2>Teste do Query Builder</h2>
-        <QueryBuilder />
-      </div>
-    </Provider>
+    <div style={{ maxWidth: 800, margin: '40px auto', padding: 24 }}>
+      <h2>Teste do Query Builder</h2>
+      <QueryBuilder />
+    </div>
   );
 }
--- app/src/components/QueryBuilder/store.ts
+++
@@ -1,6 +0,0 @@
-// app/src/components/QueryBuilder/store.ts
-import { configureStore } from '@reduxjs/toolkit';
-
-export const store = configureStore({
-  reducer: () => ({}),
-});
