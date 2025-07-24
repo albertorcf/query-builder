17135d4d90ec85270252d28da764f295d6b64582
albertorcf <albertorcf@gmail.com>
2025-07-24 18:59:38 -0300

    Remove commented-out margin and padding styles from TestePage component

--- app/src/app/teste/page.tsx
+++ app/src/app/teste/page.tsx
@@ -4,7 +4,8 @@ import QueryBuilder from "@/components/QueryBuilder/MyQueryBuilder";
 
 export default function TestePage() {
   return (
-    <div style={{ maxWidth: 800, margin: '40px auto', padding: 24 }}>
+    /*<div style={{ maxWidth: 800, margin: '40px auto', padding: 24 }}>*/
+    <div style={{ maxWidth: 800 }}>
       <h2>Teste do Query Builder</h2>
       <QueryBuilder />
     </div>
