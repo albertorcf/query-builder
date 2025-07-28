26945755f226b5ed9f6cf77e2ab863246c7fae86
albertorcf <albertorcf@gmail.com>
2025-07-28 18:45:24 -0300

    Remove redundant CSS import from MyQueryBuilder component and add it to the page for better organization

--- app/src/app/teste/page.tsx
+++ app/src/app/teste/page.tsx
@@ -7,6 +7,7 @@ import type { Config, ImmutableTree } from '@react-awesome-query-builder/antd';
 import { BasicConfig } from '@react-awesome-query-builder/antd';
 import { Utils as QbUtils } from '@react-awesome-query-builder/antd';
 import { AntdConfig, AntdWidgets } from '@react-awesome-query-builder/antd';
+import '@react-awesome-query-builder/antd/css/styles.css';
 import "@/components/QueryBuilder/MyQueryBuilder.css";
 
 // ============================================================================
--- app/src/components/QueryBuilder/MyQueryBuilder.tsx
+++ app/src/components/QueryBuilder/MyQueryBuilder.tsx
@@ -4,7 +4,6 @@ import { useState, useEffect } from 'react'
 import type { JsonGroup, Config, ImmutableTree, BuilderProps } from '@react-awesome-query-builder/antd'; // for TS example
 import { Query, Builder, Utils as QbUtils } from '@react-awesome-query-builder/antd';
 import { AntdConfig, AntdWidgets } from '@react-awesome-query-builder/antd';
-import '@react-awesome-query-builder/antd/css/styles.css';
 //import './MyQueryBuilder.css'
 
 //const InitialConfig = AntdConfig;
