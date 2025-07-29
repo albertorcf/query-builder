67ab18cee096c02398cbac94ae50e78bc24d4828
albertorcf <albertorcf@gmail.com>
2025-07-29 18:40:19 -0300

    Move "+ Add group" button CSS to a new file and update import in page.tsx

--- app/src/app/action/page.tsx
+++ app/src/app/action/page.tsx
@@ -9,7 +9,7 @@ import { Utils as QbUtils } from '@react-awesome-query-builder/antd';
 import { AntdConfig, AntdWidgets } from '@react-awesome-query-builder/antd';
 import '@react-awesome-query-builder/antd/css/styles.css';
 import "@/components/QueryBuilder/MyQueryBuilder.css";
-import "./hideAddGroupButton.css";
+import "./query-builder-actions.css";
 
 // ============================================================================
 // ⚙️ Configuração do Query Builder
