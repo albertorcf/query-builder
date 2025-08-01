171a6bd4d23797754fbdea28c0537cb91e30f449
albertorcf <albertorcf@gmail.com>
2025-08-01 18:16:48 -0300

    Enhance button styles in Home component: Update hover effects and add background colors for Conditions and Actions links

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -55,13 +55,19 @@ export default function Home() {
         <div className="flex gap-4 items-center flex-col sm:flex-row mt-3">
           <Link
             href="/condition"
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
+            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center
+              hover:bg-[#e3f0fa] dark:hover:bg-[#1a1a1a] hover:border-transparent
+              font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]
+              bg-[rgba(0,123,255,0.08)]"
           >
             ❓ Conditions
           </Link>
           <Link
             href="/action"
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
+            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center
+              hover:bg-[#ffe7e7] dark:hover:bg-[#1a1a1a] hover:border-transparent
+              font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]
+              bg-[rgba(255,0,0,0.07)]"
           >
             ⚡ Actions
           </Link>
